ca596b271cf47be42fd592fb2dc8244c13891ea2
Bilaal Latif <bilaallatif@outlook.com>
2023-11-08 23:32:09 +0000

    Created template Angular app and set manual API endpoint

---
+++ ClientApp/proxy.conf.js
@@ -0,0 +1,23 @@
+const { env } = require('process');
+
+// const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+//   env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:52497';
+
+// Manually set endpoint for API
+const target = 'https://localhost:7218'
+
+const PROXY_CONFIG = [
+  {
+    context: [
+      "/weatherforecast",
+   ],
+    proxyTimeout: 10000,
+    target: target,
+    secure: false,
+    headers: {
+      Connection: 'Keep-Alive'
+    }
+  }
+]
+
+module.exports = PROXY_CONFIG;
